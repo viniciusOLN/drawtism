7704aa1b95b7a8862320337623d4ff4b407ad657
ViniciusOLN <vinicius853o@gmail.com>
2023-10-15 00:16:46 -0300

    adicionando mensagem explicativa da tela

--- lib/app/features/photopage/presentation/photopage.dart
+++ lib/app/features/photopage/presentation/photopage.dart
@@ -2,6 +2,7 @@ import 'package:camera_camera/camera_camera.dart';
 import 'package:drawtism/app/features/choosepage/presentation/choosepage.dart';
 import 'package:drawtism/app/features/photopage/presentation/controllers/photo_controller.dart';
 import 'package:drawtism/app/features/photopage/previewpage/presentation/preview_page.dart';
+import 'package:drawtism/app/global/utils/deviceUtils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -14,6 +15,8 @@ class PhotoPage extends StatelessWidget {
       () => PhotoController(),
     );
     final controllerReference = Get.find<PhotoController>();
+    double width = DeviceUtils.width(context);
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -30,11 +33,43 @@ class PhotoPage extends StatelessWidget {
           },
         ),
       ),
-      body: CameraCamera(
-        onFile: (file) {
-          print(file);
-          controllerReference.showPreview(context, file);
-        },
+      body: Stack(
+        children: [
+          CameraCamera(
+            onFile: (file) {
+              print(file);
+              controllerReference.showPreview(context, file);
+            },
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color: Color.fromARGB(81, 0, 0, 0),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(6),
+                    child: Text(
+                      "Tire foto do desenho que você fez!",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: width / 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
